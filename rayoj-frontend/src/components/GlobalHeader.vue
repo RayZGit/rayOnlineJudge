15230e27fd21724995e9c7f11e499d608a336721
RayZGit <ruizhoudy@gmail.com>
2024-01-07 23:48:22 -0800

    Finish the BasicLayout for OJ

---
+++ rayoj-frontend/src/components/GlobalHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="globalHeader">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/logo.svg" />
+          <div class="title-bar-tittle">Ray - OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+.logo {
+  height: 48px;
+}
+.title-bar-tittle {
+  color: #444;
+  margin-left: 16px;
+}
+</style>
---
+++ rayoj-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header"> <GlobalHeader /> </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer"> RayOJ by Ray Zhou</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #eee, #000);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+}
+</style>
+
+<script>
+import { defineComponent } from "vue";
+import GlobalHeader from "@/components/GlobalHeader.vue";
+
+export default defineComponent({
+  components: { GlobalHeader },
+});
+</script>
